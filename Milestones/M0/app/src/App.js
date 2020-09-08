d99e167a12d2e08b065548a4e15f7979fec36677
xo28122000 <jainam.2000@yahoo.com>
2020-09-08 11:53:00 -0400

    scafolding screens

--- Milestones/M0/app/src/App.js
+++ Milestones/M0/app/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import HomeScreen from "./screens/Home";
+import ContactScreen from "./screens/Contact";
+import AboutScreen from "./screens/About";
+import ProfileScreen from "./screens/Profile";
+
 import "./App.css";
 
 const App = props => {
@@ -8,9 +13,18 @@ const App = props => {
     <div id="App-root">
       <Router basename="/">
         <Switch>
-          {/* <Route path="/business">
-            <BusinessScreen />
-          </Route> */}
+          <Route path="/profile">
+            <ProfileScreen />
+          </Route>
+          <Route path="/contact">
+            <ContactScreen />
+          </Route>
+          <Route path="/about">
+            <AboutScreen />
+          </Route>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
         </Switch>
       </Router>
     </div>
---
+++ Milestones/M0/app/src/screens/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+const HomeScreen = () => {
+  return (
+    <div
+      style={{ width: "100vw", height: "100vh", backgroundColor: "ButtonFace" }}
+    ></div>
+  );
+};
+
+export default HomeScreen;
