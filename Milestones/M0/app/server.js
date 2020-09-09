cd12d3d3ad06a3815d56bf02025c5ef6d0483feb
xo28122000 <jainam.2000@yahoo.com>
2020-09-09 03:54:13 -0400

    completed contact screen, added morgan and limiter

--- Milestones/M0/app/server.js
+++ Milestones/M0/app/server.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const helmet = require("helmet");
 const hpp = require("hpp");
+const morgan = require("morgan");
+const rateLimiter = require("express-rate-limit");
 
 const db = require("./db");
 
@@ -12,11 +14,20 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(hpp());
+app.use(morgan("tiny"));
 
-app.post("/contact", async (req, res) => {
-  if (!req.body || !req.body.name || !req.body.email || !req.body.description) {
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 30,
+  message:
+    "Too many requests created from this IP, please try again after 15 mins"
+});
+
+app.post("/contact", apiLimiter, async (req, res) => {
+  if (!req.body.name && !req.body.email && !req.body.description) {
     return res.send({ error: true });
   }
+
   try {
     await db.addUser(req.body.name, req.body.email, req.body.description);
   } catch (error) {
