b19fdc210fdf3b24a8f6578f215c18acd0354bc5
Ritesh <riteshpanta9@gmail.com>
2020-09-10 20:04:56 -0700

    updated

--- Milestones/M0/app/src/screens/About.js
+++ Milestones/M0/app/src/screens/About.js
@@ -10,7 +10,7 @@ import leftArrow from "../assets/left-arrow.svg";
 import rightArrow from "../assets/right-arrow.svg";
 import joseProfileImage from "../assets/jose-profile-image.JPG";
 import jainamProfileImage from "../assets/jainam-profile-image.jpeg";
-import jainamProfileImage from "../assets/riteshProfileImage-profile-image.jpeg";
+import riteshProfileImage from "../assets/ritesh-profile-image.jpeg";
 
 import placeholder from "../assets/placeholder.jpg";
 
