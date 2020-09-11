4029167cc76b5b80e8ff1ca1ee938474fe7fd860
Jose H Gonzalez <gonzalezhilariojose@gmail.com>
2020-09-10 20:35:39 -0700

    fixed minor UI bugs

--- Milestones/M0/app/src/screens/Profile.js
+++ Milestones/M0/app/src/screens/Profile.js
@@ -19,74 +19,73 @@ const ProfileScreen = props => {
       {props && !props.profile ? (
         <Redirect to="/about" />
       ) : (
-        <div>
-          {/* use this div as your main parent div, dont use props.prfile outside this div or it'll give you errors */}
-          <h1 className="name-header">
-            {props.profile.firstName}
-            <br />
-            {props.profile.lastName}
-          </h1>
-          <p className="position-header">{props.profile.position}</p>
-          {/* MAIN FLEX CONTAINER  */}
-          <div className="profile-container">
-            {/* TEAM MEMBER FLEX CARD #1*/}
-            <div className="profile-member-card">
-              {/* TEAM MEMBER PROFILE IMAGE */}
-              <div>
-                <img
-                  className="member-image"
-                  src={props.profile.imgSrc}
-                  alt="Jose H. Gonzalez Profile Image"
-                />
-              </div>
-              {/* TEAM MEMBER SOCIAL LINKS */}
-              <div className="social-links-container">
-                <a className="social-icon" href={props.profile.emailLink}>
-                  email
+          <div>
+            {/* use this div as your main parent div, dont use props.prfile outside this div or it'll give you errors */}
+            <h1 className="name-header">
+              {props.profile.firstName}
+              <br />
+              {props.profile.lastName}
+            </h1>
+            <p className="position-header">{props.profile.position}</p>
+            {/* MAIN FLEX CONTAINER  */}
+            <div className="profile-container">
+              {/* TEAM MEMBER FLEX CARD #1*/}
+              <div className="profile-member-card">
+                {/* TEAM MEMBER PROFILE IMAGE */}
+                <div>
                   <img
-                    classNameName="icon"
-                    src={emailIcon}
-                    alt="Right Arrow Button"
+                    className="member-image"
+                    src={props.profile.imgSrc}
+                    alt={props.profile.name + "Profile Image"}
                   />
-                </a>
-                {/* making bacebook optional */}
-                {props.profile.facebookLink ? (
-                  <a className="social-icon" href={props.profile.facebookLink}>
+                </div>
+                {/* TEAM MEMBER SOCIAL LINKS */}
+                <div className="social-links-container">
+                  <a className="social-icon" href={props.profile.emailLink}>
                     <img
-                      classNameName="icon"
-                      src={facebookIcon}
+                      className="icon"
+                      src={emailIcon}
                       alt="Right Arrow Button"
                     />
                   </a>
-                ) : null}
-                <a className="social-icon" href={props.profile.githubLink}>
-                  <img
-                    classNameName="icon"
-                    src={githubIcon}
-                    alt="Right Arrow Button"
-                  />
-                </a>
-                <a className="social-icon" href={props.profile.linkedinLink}>
-                  <img
-                    classNameName="icon"
-                    src={linkedinIcon}
-                    alt="Right Arrow Button"
-                  />
-                </a>
-              </div>
-              {/* TEAM MEMBER NAME & POSITION */}
-              <div>
-                <p className="member-introduction">
-                  {props.profile.aboutMe_one}
-                </p>
-                <p className="member-introduction">
-                  {props.profile.aboutMe_two}
-                </p>
+                  {/* making facebook optional */}
+                  {props.profile.facebookLink ? (
+                    <a className="social-icon" href={props.profile.facebookLink}>
+                      <img
+                        className="icon"
+                        src={facebookIcon}
+                        alt="Right Arrow Button"
+                      />
+                    </a>
+                  ) : null}
+                  <a className="social-icon" href={props.profile.githubLink}>
+                    <img
+                      className="icon"
+                      src={githubIcon}
+                      alt="Right Arrow Button"
+                    />
+                  </a>
+                  <a className="social-icon" href={props.profile.linkedinLink}>
+                    <img
+                      className="icon"
+                      src={linkedinIcon}
+                      alt="Right Arrow Button"
+                    />
+                  </a>
+                </div>
+                {/* TEAM MEMBER NAME & POSITION */}
+                <div>
+                  <p className="member-introduction">
+                    {props.profile.aboutMe_one}
+                  </p>
+                  <p className="member-introduction">
+                    {props.profile.aboutMe_two}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
     </div>
   );
 };
