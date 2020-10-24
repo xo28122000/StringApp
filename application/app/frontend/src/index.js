016e967f8da3ecb7df52586a94e77aff2445fdd1
alfredopastasauce <45408032+alfredopastasauce@users.noreply.github.com>
2020-10-24 12:02:20 -0700

    changed redux store to work with devtool on chrome and also add new file for components btns, search, cards, modal,spinner

--- application/app/frontend/src/index.js
+++ application/app/frontend/src/index.js
@@ -4,10 +4,23 @@ import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter as Router } from "react-router-dom";
+import reducerExample from './redux/Reducer/reducer';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers({
+  example: reducerExample
+})
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+
 
 ReactDOM.render(
   <Provider store={store}>
