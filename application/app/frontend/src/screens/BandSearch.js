423dce498d63b501f6772407fcb4984c3541f78a
xo28122000 <jainam.2000@yahoo.com>
2020-10-25 15:06:05 -0400

    modal form for createBand

--- application/app/frontend/src/screens/BandSearch.js
+++ application/app/frontend/src/screens/BandSearch.js
@@ -1,19 +1,131 @@
-import React from "react";
+import React, { useState } from "react";
+
+import {
+  Button,
+  Modal,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText
+} from "reactstrap";
+
 import BandSearchBar from "../components/Searchbar/BandSearchBar";
 import BandCard from "../components/Cards/BandCard";
 
 const BandSearch = () => {
-  const [bands, setBands] = React.useState([
+  const [bands, setBands] = useState([
     { name: "band1", type: "sometype" },
     { name: "band2", type: "blah" },
     { name: "band3", type: "sometype" },
     { name: "band4", type: "sometype" }
   ]);
+
+  const [createBandModal, setCreateBandModal] = useState(false);
+
+  const toggleCreateBandModal = () => setCreateBandModal(!createBandModal);
+
   return (
     <div>
-      BandSearch
+      <Modal
+        isOpen={createBandModal}
+        toggle={toggleCreateBandModal}
+        backdrop={false}
+      >
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="with a placeholder"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="password placeholder"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleSelect">Select</Label>
+              <Input type="select" name="select" id="exampleSelect">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleSelectMulti">Select Multiple</Label>
+              <Input
+                type="select"
+                name="selectMulti"
+                id="exampleSelectMulti"
+                multiple
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleText">Text Area</Label>
+              <Input type="textarea" name="text" id="exampleText" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleFile">File</Label>
+              <Input type="file" name="file" id="exampleFile" />
+              <FormText color="muted">
+                This is some placeholder block-level help text for the above
+                input. It's a bit lighter and easily wraps to a new line.
+              </FormText>
+            </FormGroup>
+            <FormGroup tag="fieldset">
+              <legend>Radio Buttons</legend>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" /> Option one is this and
+                  that—be sure to include why it's great
+                </Label>
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" /> Option two can be
+                  something else and selecting it will deselect option one
+                </Label>
+              </FormGroup>
+              <FormGroup check disabled>
+                <Label check>
+                  <Input type="radio" name="radio1" disabled /> Option three is
+                  disabled
+                </Label>
+              </FormGroup>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" /> Check me out
+              </Label>
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+        </ModalBody>
+      </Modal>
+
       <div>
         <BandSearchBar />
+        <Button color="danger" onClick={toggleCreateBandModal}>
+          open modal
+        </Button>
       </div>
       <div
         style={{
