f685aae8344638844ec9524152717518fa6c4922
xo28122000 <jainam.2000@yahoo.com>
2020-11-08 12:42:14 -0500

    Update isUser.js

--- application/app/backend/helpers/middlewares/isUser.js
+++ application/app/backend/helpers/middlewares/isUser.js
@@ -2,8 +2,8 @@ const isUser = (req, res, next) => {
   if (req.user) {
     next();
   } else {
-    res.send({ success: "false" });
+    res.send({ success: false });
   }
 };
 
-module.exports = { isUser };
+module.exports = isUser;
