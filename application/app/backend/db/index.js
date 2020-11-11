fc22e0ccccb91dd806a4af88634252ed66bd6332
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-11-10 18:14:20 -0800

    initialization queries for mySQL tables (not including administrator)

--- application/app/backend/db/index.js
+++ application/app/backend/db/index.js
@@ -47,6 +47,100 @@ pool.getConnection(async function (err, connection) {
     function (error, result, fields) {}
   );
 
+  connection.query(
+    "CREATE TABLE IF NOT EXISTS BAND(" +
+      "bandId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+      "name varchar(100) NOT NULL, " +
+      "logoImageUrl varchar(45), " +
+      "location varchar(500), " +
+      "locationLat decimal(30,15), " +
+      "locationLong decimal(30,15), " +
+      "genre varchar(100), " +
+      "isLookingForMember BOOLEAN" +
+      " );",
+    function (error, result, fields) {}
+  );
+
+  connection.query(
+    "CREATE TABLE IF NOT EXISTS BANDMEMBERS(" +
+      "bandMemberId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+      "isBandAdmin BOOLEAN, " +
+      "role varchar(45), " +
+      "dateJoined varchar(45), " + //TODO shouldn't this be a DATE, like in the invitations table?
+      "userId INT, " +
+      "bandId INT, " +
+      "FOREIGN KEY (userId) REFERENCES stringaccount(userId) ON UPDATE CASCADE ON DELETE CASCADE, " +
+      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+      " );",
+    function (error, result, fields) {}
+  );
+
+  connection.query(
+    "CREATE TABLE IF NOT EXISTS INVITATIONS(" +
+      "inviteId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+      "message varchar(150), " +
+      "dateSent DATE, " +
+      "userId INT, " +
+      "bandId INT, " +
+      "FOREIGN KEY (userId) REFERENCES stringaccount(userId) ON UPDATE CASCADE ON DELETE CASCADE, " +
+      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+      " );",
+    function (error, result, fields) {}
+  );
+
+  connection.query(
+    "CREATE TABLE IF NOT EXISTS BANDPOSTS(" +
+      "bandPostId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+      "media varchar(400), " +
+      "title varchar(45), " +
+      "description varchar(400), " +
+      "bandId INT, " +
+      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+      " );",
+    function (error, result, fields) {}
+  );
+
+  connection.query(
+    "CREATE TABLE IF NOT EXISTS REPERTOIRE(" +
+      "repId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+      "songName varchar(45), " +
+      "runTime varchar(45), " +
+      "genre varchar(45), " +
+      "link varchar(150), " +
+      "bandId INT, " +
+      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+      " );",
+    function (error, result, fields) {}
+  );
+
+  connection.query(
+    "CREATE TABLE IF NOT EXISTS EVENTS(" +
+      "eventId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+      "title varchar(45), " +
+      "description varchar(400), " +
+      "date DATE, " +
+      "startTime TIME, " +
+      "endTime TIME, " +
+      "location varchar(500), " +
+      "locationLat decimal(30,15), " +
+      "locationLong decimal(30,15), " +
+      "bandId INT, " +
+      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+      " );",
+    function (error, result, fields) {}
+  );
+
+  connection.query(
+    "CREATE TABLE IF NOT EXISTS SETS(" +
+      "setId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+      "songName varchar(45), " +
+      "runTime varchar(45), " +
+      "eventId INT, " +
+      "FOREIGN KEY (eventId) REFERENCES EVENTS(eventId) ON UPDATE CASCADE ON DELETE CASCADE" +
+      " );",
+    function (error, result, fields) {}
+  );
+
   // connection.query("DROP TABLE MOCKBAND;", function(error, result, fields) {});
   connection.release();
 });
