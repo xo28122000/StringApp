127dd7002f171708a52bff89d7867dcc84a0be6b
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2020-11-11 22:18:15 -0800

    create events mostly working
    need to get locationLat and locationLong from location via geocode middleware for SQL query, otherwise inserts into table fine

--- application/app/backend/controllers/bandController/bandController.js
+++ application/app/backend/controllers/bandController/bandController.js
@@ -2,6 +2,7 @@ const bandQueries = require("../../db/queries/band.js");
 
 const awsS3 = require("../../lib/aws/s3");
 
+//TODO need to fix middleware
 const createEvent = (req, res) => {
   if (
     !req.body.bandId ||
@@ -18,8 +19,34 @@ const createEvent = (req, res) => {
       error: "fields missing for createEvent",
     });
   }
+  //TODO middleware fix: need to get correct bandId somehow?
+  //or is this a frontEnd problem?
 
-  bandQueries.createEvent();
+  //TODO middleware fix: also need to get location for correct input
+  //for sql queries: locationLat and locationLong
+  //which are decimal(30,15)
+  bandQueries
+    .createEvent(
+      req.body.title,
+      req.body.description,
+      req.body.date,
+      req.body.startTime,
+      req.body.endTime,
+      req.body.location,
+      req.body.bandId
+    )
+    .then((retObj) => {
+      console.log("successful creation of event");
+      return res.send({ success: true });
+    })
+    .catch((err) => {
+      console.log("internal error creating event");
+      console.log(err);
+      return res.send({
+        success: false,
+        error: "internal error when trying to create event",
+      });
+    });
 };
 
 module.exports = { createEvent };
--- application/app/backend/db/queries/band.js
+++ application/app/backend/db/queries/band.js
@@ -1,21 +1,6 @@
 const pool = require("../index");
 let bandQueries = {};
 
-//TODO fix this SQL query:
-//(node:14588) UnhandledPromiseRejectionWarning:
-//Error: ER_BAD_FIELD_ERROR: Unknown column 'undefined' in 'field list'
-//something wrong with the Promise, or pool.query
-/**
- * (node:14588) UnhandledPromiseRejectionWarning: Unhandled promise rejection.
- * This error originated either by throwing inside of an async function
- * without a catch block, or by rejecting a promise which was not handled
- * with .catch(). To terminate the node process on unhandled promise rejection,
- * use the CLI flag `--unhandled-rejections=strict`
- * (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
- * (node:14588) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated.
- * In the future, promise rejections that are not handled will terminate
- * the Node.js process with a non-zero exit code.
- * */
 bandQueries.createEvent = (
   title,
   description,
@@ -27,7 +12,7 @@ bandQueries.createEvent = (
 ) => {
   return new Promise((resolve, reject) => {
     pool.query(
-      `INSERT INTO EVENTS (title, description, date, startTime, endTime, location, bandId) VALUES('${title}', '${description}', ${date}, '${startTime}', '${endTime}', '${location}', '${bandId}')`,
+      `INSERT INTO EVENTS (title, description, date, startTime, endTime, location, bandId) VALUES('${title}', '${description}', '${date}', '${startTime}', '${endTime}', '${location}', '${bandId}')`,
       (err, results) => {
         if (err) {
           return reject(err);
