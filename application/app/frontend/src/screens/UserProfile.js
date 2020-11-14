b1e5295b212e700ba231ebc2a2ac160a073c38d0
leonidnovoselov <leo.bruno@inbox.ru>
2020-11-14 11:29:20 -0800

    UserProf v 0.2

--- application/app/frontend/src/screens/UserProfile.js
+++ application/app/frontend/src/screens/UserProfile.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 import BandCard from "../components/Cards/BandCard";
-import { Row, Col, Container, Badge, NavLink } from "reactstrap";
+import { Row, Col, Container, Badge, NavLink, Card, Button } from "reactstrap";
+import { scroller } from "react-scroll";
 
 const UserProfileScreen = () => {
   const [bands, setBands] = useState([
@@ -22,10 +23,50 @@ const UserProfileScreen = () => {
 
   return (
     <Container>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: 0,
+          width: "70vw",
+          maxWidth: 800,
+          minWidth: 300,
+          backgroundColor: "#000000",
+        }}
+      >
+        <div
+          style={{
+            marginRight: 0,
+            maxWidth: 800,
+            fontSize: 50,
+            color: "white",
+          }}
+        >
+          John Lennon
+        </div>
+      </div>
       <Row>
-        <Col sm={{ size: "auto", offset: 1 }}>
-          <Container>
-            <nav>
+        <Col sm={{ size: "auto", offset: 0 }}>
+          <img src="../logo512.png" />
+          <Card
+            body
+            style={{
+              marginTop: 50,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            <nav
+              style={{
+                fontSize: 20,
+                fontWeight: 500,
+                paddingLeft: 40,
+                paddingRight: 40,
+              }}
+            >
               <div>
                 Instagram:<NavLink>http</NavLink>
               </div>
@@ -42,21 +83,51 @@ const UserProfileScreen = () => {
                 Apple music:<NavLink>http</NavLink>
               </div>
             </nav>
-          </Container>
+          </Card>
         </Col>
-        <Col sm={{ size: "auto", offset: 1 }}>
-          <Row>
-            <Badge color="dark">Rock</Badge>
-            <Badge color="dark">The Beatles</Badge>
-            <Badge color="dark">Band Manager</Badge>
+        <Col sm={{ size: "auto", offset: 0 }}>
+          <Row
+            style={{
+              marginTop: 50,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            <Badge color="dark" pill>
+              Rock
+            </Badge>
+            <Badge color="dark" pill>
+              The Beatles
+            </Badge>
+            <Badge color="dark" pill>
+              Band Manager
+            </Badge>
           </Row>
-          <Row>
+          <Row
+            style={{
+              marginTop: 10,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
             <Badge color="dark">San Francisco</Badge>
             <button>Account settings</button>
           </Row>
-          <Row>
+          <Row
+            style={{
+              marginTop: 20,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexWrap: "wrap",
+            }}
+          >
             <h1>Bands</h1>
-            <button>plus</button>
+            <button>Add</button>
           </Row>
           <div>
             <div
