50019b761f4ffffd04ab30c58293f93c402507b2
xo28122000 <jainam.2000@yahoo.com>
2020-11-18 17:07:55 -0500

    revert deleted merge conflicts

---
+++ application/app/frontend/src/components/Navbar/index.js
@@ -0,0 +1,180 @@
+import React, { useState, useEffect } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import { login, logout } from "../../redux/Actions/actions";
+import { Link, withRouter } from "react-router-dom";
+import { Button, Modal, ModalHeader, ModalBody, Input } from "reactstrap";
+
+import "./navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+import StringLogo from "../../assets/StringLogo.png";
+import Axios from "axios";
+
+const Navbar = props => {
+  const dispatch = useDispatch();
+
+  const userObj = useSelector(state => state.userObj);
+
+  const [loginModal, setLoginModal] = useState(false);
+  const toggleLoginModal = () => setLoginModal(!loginModal);
+
+  const sendlogin = () => {
+    let email = document.getElementById("loginEmail").value;
+    let password = document.getElementById("loginPassword").value;
+
+    if (!email || !password) {
+      alert("email and password are required.");
+    } else {
+      Axios.post("/api/auth/login", { email, password })
+        .then(res => {
+          console.log(res);
+          dispatch(login({ email, password }));
+          toggleLoginModal();
+        })
+        .catch(err => {
+          alert("some error occured. Please try again later");
+        });
+    }
+  };
+
+  const triggerLogout = () => {
+    dispatch(logout());
+  };
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleResize() {
+      setScreenWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        // height: screenWidth > 450 ? 70 : 40,
+        backgroundColor: "#000000",
+        paddingRight: 20
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center"
+        }}
+      >
+        <Link to="/">
+          <img
+            src={StringLogo}
+            alt="logo"
+            style={{ height: screenWidth > 450 ? 70 : 40 }}
+          />
+        </Link>
+        {screenWidth > 450 ? (
+          <div style={{ marginLeft: 20 }}>
+            <a href="/#explore" className="navLink">
+              Explore
+            </a>
+          </div>
+        ) : null}
+      </div>
+
+      {!userObj ? (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ marginRight: 20 }}>
+            <a
+              className="navLink"
+              onClick={() => {
+                toggleLoginModal();
+              }}
+            >
+              Log in
+            </a>
+          </div>
+          <Button
+            className="navButton"
+            onClick={() => {
+              props.history.push("/register");
+            }}
+          >
+            Sign up
+          </Button>
+        </div>
+      ) : (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Link
+            to="/profile"
+            className="navLink"
+            style={{
+              borderColor: "#ffffff",
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderRadius: 25,
+              height: 50,
+              width: 50,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginRight: 20
+            }}
+          >
+            <FontAwesomeIcon icon={faUser} style={{ fontSize: 25 }} />
+          </Link>
+          <Button
+            className="navButton"
+            onClick={() => {
+              triggerLogout();
+            }}
+          >
+            Logout
+          </Button>
+        </div>
+      )}
+
+      <Modal isOpen={loginModal} toggle={toggleLoginModal} backdrop="static">
+        <ModalHeader toggle={toggleLoginModal}>Login</ModalHeader>
+        <ModalBody>
+          <div style={{ marginBottom: 20 }}>
+            <Input
+              type="email"
+              id="loginEmail"
+              placeholder="email"
+              style={{ borderRadius: 20 }}
+            />
+          </div>
+          <div style={{ marginBottom: 20 }}>
+            <Input
+              type="password"
+              id="loginPassword"
+              placeholder="password"
+              style={{ borderRadius: 20 }}
+            />
+          </div>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              style={{
+                fontSize: 18,
+                backgroundColor: "#000000",
+                borderRadius: 10
+              }}
+              onClick={() => {
+                sendlogin();
+              }}
+            >
+              Login
+            </Button>
+          </div>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+};
+
+export default withRouter(Navbar);
---
+++ application/app/frontend/src/components/Searchbar/BandSearchBar.js
@@ -0,0 +1,433 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  CustomInput
+} from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCoffee,
+  faSearch,
+  faFilter,
+  faPlus,
+  faSlidersH
+} from "@fortawesome/free-solid-svg-icons";
+import Axios from "axios";
+
+const BandSearchBar = props => {
+  const genreOptions = ["All", "Rock", "Acoustic", "Jazz", "Pop", "Hip Hop"];
+
+  const [filterBandModal, setFilterBandModal] = useState(false);
+  const toggleFilterBandModal = () => setFilterBandModal(!filterBandModal);
+
+  const [filterGenreDDOpen, setFilterGenreDDOpen] = useState(false);
+  const toggleFilterGenreDDOpen = () =>
+    setFilterGenreDDOpen(!filterGenreDDOpen);
+
+  const [searchName, setSearchName] = useState("");
+  const [filterType, setFilterType] = useState(null);
+  const [filterNumMembers, setFilterNumMembers] = useState(1);
+
+  const searchBands = () => {
+    let params = {};
+    params["name"] = !searchName || searchName.length <= 0 ? null : searchName;
+    params["type"] = !filterType || filterType.length <= 0 ? null : filterType;
+    params["numMembers"] =
+      !filterNumMembers || !isNaN(filterNumMembers) || filterNumMembers <= 0
+        ? 1
+        : filterType;
+
+    Axios.post("/api/mockBand/searchBand", params)
+      .then(resp => {
+        if (resp.data.success) {
+          props.setBands(resp.data.result);
+        }
+      })
+      .catch(err => {});
+  };
+
+  return (
+    <div>
+      <Modal
+        isOpen={filterBandModal}
+        toggle={toggleFilterBandModal}
+        backdrop={"static"}
+      >
+        <ModalHeader toggle={toggleFilterBandModal}>Filter Bands:</ModalHeader>
+        <ModalBody>
+          <div style={{ marginBottom: 15 }}>
+            <div style={{ display: "flex" }}>Location:</div>
+            <div style={{ marginLeft: 20, marginRight: 20 }}>
+              <Input
+                id="filterLocationStreet"
+                placeholder="street"
+                style={{
+                  borderRadius: 25,
+                  margin: 5
+                }}
+              />
+              <div style={{ display: "flex" }}>
+                <Input
+                  id="filterLocationCity"
+                  placeholder="city"
+                  style={{
+                    borderRadius: 25,
+                    margin: 5
+                  }}
+                />
+
+                <Input
+                  id="filterLocationState"
+                  placeholder="state"
+                  style={{
+                    borderRadius: 25,
+                    margin: 5
+                  }}
+                />
+
+                <Input
+                  id="filterLocationZip"
+                  placeholder="zip"
+                  style={{
+                    borderRadius: 25,
+                    margin: 5
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: 20
+            }}
+          >
+            Genre:
+            <Dropdown
+              isOpen={filterGenreDDOpen}
+              toggle={toggleFilterGenreDDOpen}
+              style={{ marginLeft: 20 }}
+            >
+              <DropdownToggle caret style={{ backgroundColor: "#000000" }}>
+                {filterType ? filterType : "All"}
+              </DropdownToggle>
+              <DropdownMenu>
+                {genreOptions.map(genreOption => (
+                  <DropdownItem
+                    key={genreOption}
+                    onClick={() => {
+                      setFilterType(genreOption === "All" ? null : genreOption);
+                    }}
+                  >
+                    {genreOption}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: 20
+            }}
+          >
+            <FormGroup check>
+              <Label check>
+                <Input id="filterBandsAccepting" type="checkbox" /> Only Bands
+                looking for members
+              </Label>
+            </FormGroup>
+          </div>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              onClick={() => {
+                toggleFilterBandModal();
+              }}
+              style={{
+                borderRadius: 20,
+                textAlign: "center",
+                backgroundColor: "#000000"
+              }}
+            >
+              Apply filters
+            </Button>
+          </div>
+        </ModalBody>
+      </Modal>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#000000",
+          padding: 20
+        }}
+      >
+        <div
+          style={{
+            color: "#ffffff",
+            fontFamily: "roboto",
+            fontWeight: 600,
+            fontSize: 35,
+            padding: 25
+          }}
+        >
+          Explore Bands Nearby
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            paddingBottom: 25
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#ffffff",
+              borderRadius: 25,
+              padding: 5,
+              marginRight: 25
+            }}
+          >
+            <Input
+              value={searchName}
+              onChange={ev => {
+                setSearchName(ev.target.value);
+              }}
+              placeholder="Search..."
+              style={{
+                width: "30vw",
+                minWidth: 100,
+                marginRight: 10,
+                borderWidth: 0,
+                borderRadius: 25
+              }}
+            />
+
+            <Button
+              onClick={() => {
+                searchBands();
+              }}
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 20,
+                textAlign: "center",
+                backgroundColor: "#000000"
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faSearch}
+                style={{ width: 15, height: 15 }}
+              />
+            </Button>
+          </div>
+          <div>
+            <Button
+              onClick={() => {
+                toggleFilterBandModal();
+              }}
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: 8,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#ffffff"
+              }}
+            >
+              <FontAwesomeIcon
+                icon={faSlidersH}
+                color="#CB0086"
+                style={{ fontSize: 20 }}
+              />
+            </Button>
+          </div>
+        </div>
+        {/* <Button
+          onClick={toggleCreateBandModal}
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+            textAlign: "center",
+            backgroundColor: "#000000"
+          }}
+        >
+          <FontAwesomeIcon icon={faPlus} style={{ width: 15, height: 15 }} />
+        </Button> */}
+      </div>
+    </div>
+  );
+};
+
+export default BandSearchBar;
+
+// create mocband modal:
+
+/* <Modal
+        isOpen={createBandModal}
+        toggle={toggleCreateBandModal}
+        backdrop={"static"}
+      >
+        <ModalHeader toggle={toggleCreateBandModal}>Create a Band</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label>Name of your Band</Label>
+              <Input
+                value={createName}
+                onChange={ev => {
+                  setCreateName(ev.target.value);
+                }}
+              />
+            </FormGroup>
+          </Form>
+          <FormGroup>
+            <Label>Type of music</Label>
+            <Dropdown isOpen={createTypeDDOpen} toggle={toggleCreateTypeDDOpen}>
+              <DropdownToggle caret>
+                {createType ? createType : "Please select a type"}
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem
+                  onClick={() => {
+                    setCreateType("rock");
+                  }}
+                >
+                  Rock
+                </DropdownItem>
+                <DropdownItem
+                  onClick={() => {
+                    setCreateType("accoustic");
+                  }}
+                >
+                  Accoustic
+                </DropdownItem>
+                <DropdownItem
+                  onClick={() => {
+                    setCreateType("jazz");
+                  }}
+                >
+                  Jazz
+                </DropdownItem>
+                <DropdownItem
+                  onClick={() => {
+                    setCreateType("pop");
+                  }}
+                >
+                  Pop
+                </DropdownItem>
+                <DropdownItem
+                  onClick={() => {
+                    setCreateType("hip hop");
+                  }}
+                >
+                  Hip Hop
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </FormGroup>
+          <FormGroup>
+            <Label>Number Of Band Members</Label>
+            <Input
+              value={createNumBandMembers}
+              onChange={ev => {
+                setCreateNumBandMembers(ev.target.value);
+              }}
+              type="number"
+              placeholder="1"
+              min={1}
+              max={30}
+              onKeyPress={ev => {
+                ev.preventDefault();
+                return false;
+              }}
+            />
+            <FormText color="muted">
+              Use the spinner to change the number
+            </FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label>Upload your Band Logo</Label>
+            <CustomInput
+              id="createBandFileInput"
+              type="file"
+              accept="image/jpg,image/jpeg,image/png"
+            />
+          </FormGroup>
+          <Button
+            onClick={() => {
+              createBand();
+            }}
+            color={"primary"}
+          >
+            Create
+          </Button>
+          <FormText color="muted">
+            You can only create a limited number of Bands in an hour.
+          </FormText>
+        </ModalBody>
+      </Modal> */
+
+//
+//
+//
+//
+
+// const createBand = () => {
+//   var imageFileInput = document.getElementById("createBandFileInput");
+//   if (
+//     !createName ||
+//     createName.length <= 0 ||
+//     !createType ||
+//     !isNaN(createNumBandMembers) ||
+//     createNumBandMembers < 1 ||
+//     imageFileInput.files.length !== 1
+//   ) {
+//     alert(
+//       "Check the input fields and retry again. All fields are compulsary."
+//     );
+//   } else if ((imageFileInput.files[0].size / 1024 / 1024).toFixed(4) > 10) {
+//     alert("Please use a file of size smaller than 10mb");
+//   } else {
+//     toggleCreateBandModal();
+//     var formData = new FormData();
+//     formData.append("imageFile", imageFileInput.files[0]);
+//     formData.append("name", createName);
+//     formData.append("type", createType);
+//     formData.append("numMembers", createNumBandMembers);
+//     Axios.post("/api/mockBand/createBand", formData, {
+//       headers: {
+//         "Content-Type": "multipart/form-data"
+//       }
+//     })
+//       .then(resp => {
+//         if (!resp.data.success) {
+//           alert("failed to create band");
+//         }
+//       })
+//       .catch(err => {
+//         alert("failed to create band");
+//       });
+//   }
+// };
