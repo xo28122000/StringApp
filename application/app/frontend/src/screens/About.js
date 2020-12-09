a60833bc7138556616ac41032d9895e1b9d20b3a
alfredopastasauce <45408032+alfredopastasauce@users.noreply.github.com>
2020-12-09 13:37:06 -0800

    about page svg illustrations

--- application/app/frontend/src/screens/About.js
+++ application/app/frontend/src/screens/About.js
@@ -1,10 +1,20 @@
 import React from "react";
+import saxophoneplay from "../assets/saxophoneplay.svg";
+import bro from "../assets/bro.svg";
 
 const About = () => {
   return (
     <div>
       About Screen: should contain what our product is, how can users use it and
       what is our goal. 
+              <div style={{ marginBottom: 20 }}>
+                <img src={saxophoneplay} style={{ width: 500, maxHeight: 500 }} />
+              </div>
+
+              <div style={{ marginBottom: 20 }}>
+                <img src={bro} style={{ width: 500, maxHeight: 500 }} />
+              </div>
+
     </div>
   );
 };
