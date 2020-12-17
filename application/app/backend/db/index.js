1146b1485639a6c9ee27b92dd82afa32d6726b13
xo28122000 <jainam.2000@yahoo.com>
2020-12-17 00:46:37 -0600

    Update index.js

--- application/app/backend/db/index.js
+++ application/app/backend/db/index.js
@@ -21,10 +21,11 @@ pool.getConnection(async function(err, connection) {
   } // not connected!
   console.log("MYSQL connection successful!");
 
-  //below line won't work since the createPool call at line 7 needs the database to exist already
-  await connection.query("CREATE DATABASE IF NOT EXISTS StringApp;");
-  await connection.query("USE StringApp;");
-  /** 
+  try {
+    //below line won't work since the createPool call at line 7 needs the database to exist already
+    await connection.query("CREATE DATABASE IF NOT EXISTS StringApp;");
+    await connection.query("USE StringApp;");
+    /** 
   await connection.query( 
     "CREATE TABLE IF NOT EXISTS MOCKBAND(" +
       "name varchar(30), " +
@@ -35,123 +36,127 @@ pool.getConnection(async function(err, connection) {
   );
   */
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS STRINGACCOUNT(" +
-      "userId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "email varchar(350) NOT NULL UNIQUE, " +
-      "password varchar(200) NOT NULL, " +
-      "name varchar(100) NOT NULL, " +
-      "profileImageUrl varchar(150), " +
-      "phoneNumber varchar(15), " +
-      "links varchar(500), " +
-      "location varchar(500), " +
-      "locationLat decimal(30,15), " +
-      "locationLong decimal(30,15), " +
-      "role varchar(50), " +
-      "genre varchar(50)" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS STRINGACCOUNT(" +
+        "userId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "email varchar(350) NOT NULL UNIQUE, " +
+        "password varchar(200) NOT NULL, " +
+        "name varchar(100) NOT NULL, " +
+        "profileImageUrl varchar(150), " +
+        "phoneNumber varchar(15), " +
+        "links varchar(500), " +
+        "location varchar(500), " +
+        "locationLat decimal(30,15), " +
+        "locationLong decimal(30,15), " +
+        "role varchar(50), " +
+        "genre varchar(50)" +
+        " );"
+    );
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS BAND(" +
-      "bandId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "name varchar(100) NOT NULL UNIQUE, " +
-      "numMembers INT DEFAULT 1, " +
-      "logoImageUrl varchar(500), " +
-      "links varchar(500), " +
-      "description varchar(500), " +
-      "location varchar(500), " +
-      "locationLat decimal(30,15), " +
-      "locationLong decimal(30,15), " +
-      "genre varchar(100), " +
-      "isLookingForMember BOOLEAN" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS BAND(" +
+        "bandId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "name varchar(100) NOT NULL UNIQUE, " +
+        "numMembers INT DEFAULT 1, " +
+        "logoImageUrl varchar(500), " +
+        "links varchar(500), " +
+        "description varchar(500), " +
+        "location varchar(500), " +
+        "locationLat decimal(30,15), " +
+        "locationLong decimal(30,15), " +
+        "genre varchar(100), " +
+        "isLookingForMember BOOLEAN" +
+        " );"
+    );
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS BANDMEMBERS(" +
-      "bandMemberId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "isBandAdmin BOOLEAN, " +
-      "role varchar(45), " +
-      "dateJoined DATE, " +
-      "userId INT, " +
-      "bandId INT, " +
-      "FOREIGN KEY (userId) REFERENCES stringaccount(userId) ON UPDATE CASCADE ON DELETE CASCADE, " +
-      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS BANDMEMBERS(" +
+        "bandMemberId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "isBandAdmin BOOLEAN, " +
+        "role varchar(45), " +
+        "dateJoined DATE, " +
+        "userId INT, " +
+        "bandId INT, " +
+        "FOREIGN KEY (userId) REFERENCES stringaccount(userId) ON UPDATE CASCADE ON DELETE CASCADE, " +
+        "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+        " );"
+    );
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS INVITATIONS(" +
-      "inviteId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "message varchar(1000), " +
-      "dateSent DATE, " +
-      "sentByBand BOOLEAN, " +
-      "userId INT, " +
-      "bandId INT, " +
-      "FOREIGN KEY (userId) REFERENCES stringaccount(userId) ON UPDATE CASCADE ON DELETE CASCADE, " +
-      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS INVITATIONS(" +
+        "inviteId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "message varchar(1000), " +
+        "dateSent DATE, " +
+        "sentByBand BOOLEAN, " +
+        "userId INT, " +
+        "bandId INT, " +
+        "FOREIGN KEY (userId) REFERENCES stringaccount(userId) ON UPDATE CASCADE ON DELETE CASCADE, " +
+        "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+        " );"
+    );
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS BANDPOSTS(" +
-      "bandPostId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "media varchar(400), " +
-      "title varchar(45), " +
-      "description varchar(400), " +
-      "bandId INT, " +
-      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS BANDPOSTS(" +
+        "bandPostId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "media varchar(400), " +
+        "title varchar(45), " +
+        "description varchar(400), " +
+        "bandId INT, " +
+        "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+        " );"
+    );
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS REPERTOIRE(" +
-      "repId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "songName varchar(45), " +
-      "runTime varchar(45), " +
-      "genre varchar(45), " +
-      "link varchar(150), " +
-      "bandId INT, " +
-      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS REPERTOIRE(" +
+        "repId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "songName varchar(45), " +
+        "runTime varchar(45), " +
+        "genre varchar(45), " +
+        "link varchar(150), " +
+        "bandId INT, " +
+        "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+        " );"
+    );
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS EVENTS(" +
-      "eventId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "title varchar(45), " +
-      "description varchar(400), " +
-      "date DATE, " +
-      "startTime TIME, " +
-      "endTime TIME, " +
-      "location varchar(500), " +
-      "locationLat decimal(30,15), " +
-      "locationLong decimal(30,15), " +
-      "bandId INT, " +
-      "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS EVENTS(" +
+        "eventId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "title varchar(45), " +
+        "description varchar(400), " +
+        "date DATE, " +
+        "startTime TIME, " +
+        "endTime TIME, " +
+        "location varchar(500), " +
+        "locationLat decimal(30,15), " +
+        "locationLong decimal(30,15), " +
+        "bandId INT, " +
+        "FOREIGN KEY (bandId) REFERENCES band(bandId) ON UPDATE CASCADE ON DELETE CASCADE" +
+        " );"
+    );
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS SETS(" +
-      "setId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "songName varchar(45), " +
-      "runTime varchar(45), " +
-      "eventId INT, " +
-      "FOREIGN KEY (eventId) REFERENCES EVENTS(eventId) ON UPDATE CASCADE ON DELETE CASCADE" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS SETS(" +
+        "setId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "songName varchar(45), " +
+        "runTime varchar(45), " +
+        "eventId INT, " +
+        "FOREIGN KEY (eventId) REFERENCES EVENTS(eventId) ON UPDATE CASCADE ON DELETE CASCADE" +
+        " );"
+    );
 
-  await connection.query(
-    "CREATE TABLE IF NOT EXISTS administratorStringAccount(" +
-      "adminUserId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
-      "email varchar(45), " +
-      "password varchar(45), " +
-      "name varchar(45), " +
-      "role varchar(45)" +
-      " );"
-  );
+    await connection.query(
+      "CREATE TABLE IF NOT EXISTS administratorStringAccount(" +
+        "adminUserId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+        "email varchar(45), " +
+        "password varchar(45), " +
+        "name varchar(45), " +
+        "role varchar(45)" +
+        " );"
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
   // connection.query("DROP TABLE MOCKBAND;", function(error, result, fields) {});
   connection.release();
 });
