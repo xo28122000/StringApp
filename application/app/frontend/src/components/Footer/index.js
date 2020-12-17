62e8056c56e76f914a1bbe09babda99e043d8db8
xo28122000 <jainam.2000@yahoo.com>
2020-12-17 15:03:43 -0600

    Update index.js

--- application/app/frontend/src/components/Footer/index.js
+++ application/app/frontend/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import { Link, withRouter } from "react-router-dom";
 
 import "./footer.css";
 
-const Footer = (props) => {
+const Footer = props => {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   useEffect(() => {
     function handleResize() {
@@ -18,63 +18,57 @@ const Footer = (props) => {
   return (
     <div
       style={{
+        width: "100%",
         display: "flex",
-        justifyContent: "space-between",
+        justifyContent: "center",
         alignItems: "center",
-        height: screenWidth > 450 ? 70 : 40,
+        padding: 40,
         backgroundColor: "#000000",
-        paddingRight: 20,
+        paddingRight: 20
       }}
     >
       <div
         style={{
           display: "flex",
           alignItems: "center",
+          justifyContent: "center"
         }}
       >
-        <Link to="/explore"></Link>
-        {screenWidth > 450 ? (
-          <div style={{ marginLeft: 20 }}>
-            <a href="/about" className="navLink">
-              About
-            </a>
-          </div>
-        ) : null}
-        {screenWidth > 450 ? (
-          <div style={{ marginLeft: 20 }}>
-            <a href="/explore#explore" className="navLink">
-              Explore
-            </a>
-          </div>
-        ) : null}
-        {screenWidth > 450 ? (
-          <div style={{ marginLeft: 20 }}>
-            <a href="/contact" className="navLink">
-              Contact Us
-            </a>
-          </div>
-        ) : null}
-        {screenWidth > 450 ? (
-          <div style={{ marginLeft: 20 }}>
-            <a href="/data_policy" className="navLink">
-              Data Policy
-            </a>
-          </div>
-        ) : null}
-        {screenWidth > 450 ? (
-          <div style={{ marginLeft: 20 }}>
-            <a href="/terms_and_conditions" className="navLink">
-              Terms and Conditions
-            </a>
-          </div>
-        ) : null}
-        {screenWidth > 450 ? (
-          <div style={{ marginLeft: 20 }}>
-            <a href="/FAQ" className="navLink">
-              FAQ
-            </a>
-          </div>
-        ) : null}
+        <div style={{ marginLeft: 20 }}>
+          <Link to="/about" className="navLink">
+            About
+          </Link>
+        </div>
+
+        <div style={{ marginLeft: 20 }}>
+          <Link to="/explore#explore" className="navLink">
+            Explore
+          </Link>
+        </div>
+
+        <div style={{ marginLeft: 20 }}>
+          <Link to="/contact" className="navLink">
+            Contact Us
+          </Link>
+        </div>
+
+        <div style={{ marginLeft: 20 }}>
+          <Link to="/data_policy" className="navLink">
+            Data Policy
+          </Link>
+        </div>
+
+        <div style={{ marginLeft: 20 }}>
+          <Link to="/terms_and_conditions" className="navLink">
+            Terms and Conditions
+          </Link>
+        </div>
+
+        <div style={{ marginLeft: 20 }}>
+          <Link to="/FAQ" className="navLink">
+            FAQ
+          </Link>
+        </div>
       </div>
     </div>
   );
